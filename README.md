c28d09e16b729ee335c027a3e6db45a12e05d943
Kernic <corentin.le_gall@outlook.fr>
2026-01-28 14:20:14 +0100

    Refurbishing project, starting with dash webpages

--- README.md
+++ README.md
@@ -4,9 +4,12 @@
 1. Create a website to manage the monthly and yearly budget.
 2. It is a tailored solution to Crédit Agricole csv extract
 3. Should be as easy as possible
+4. Should be using dash as interface
 
-## Order: 
-- [ ] Configure the database
-- [ ] Configure the website
-- [ ] Configure the upload script
-- [ ] First Graphics
+## Order:
+- [x] Create Pages
+- [ ] Create Database management
+- [ ] Implement Overview Graphiques And KPI
+- [ ] Implement Detail Page Table with KPI
+- [ ] Implement Upload Script
+- [ ] Scale projet to update / save db in cloud for sync
--- src/main.py
+++ src/main.py
@@ -1,9 +1,57 @@
-import db.database as datab
-import web.web as web
+import dash
+import dash_bootstrap_components as dbc
+from dash import dcc
 
+# Creating the Dash app
+app = dash.Dash(
+    __name__,
+    use_pages=True,
+    pages_folder="web",
+    external_stylesheets=[dbc.themes.MORPH],
+)
+
+# Creating elements for all the pages
+nav_bar = dbc.NavbarSimple(
+    children=dbc.Nav(
+        [
+            dbc.NavItem(
+                dbc.NavLink(f"{page['name']}", href=page["path"]),
+                id=f"tooltip-target{page['name']}",
+                style={"textAlign": "center"},
+            )
+            for page in dash.page_registry.values()
+        ],
+        navbar=True,
+    ),
+    brand="Budget Management App",
+    brand_href="/",
+    color="primary",
+    dark=True,
+)
+
+
+# Defining the layout that will be used for all the pages
+app.layout = dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col(nav_bar),
+            ]
+        ),
+        dbc.Row(dbc.Col(dash.page_container, style={"margin-top": "20px"})),
+        dbc.Row(
+            [
+                dbc.Tooltip(
+                    f"{page['description']}",
+                    target=f"tooltip-target{page['name']}",
+                    placement="bottom",
+                )
+                for page in dash.page_registry.values()
+            ]
+        ),
+    ]
+)
+
+# Main Run of the function
 if __name__ == "__main__":
-    info = datab.DataManagement()
-    info.init_random()
-    tmp = info.get_globals()
-    website = web.website(tmp[0][1])
-    website.run()
+    app.run(debug=True)
